12c72cead3490c1d8a11bbed1aa156fefcd90e83
sarahduv <sarah.duvdevani@gmail.com>
2020-03-29 13:25:12 -0700

    added colorHelper to generate scale of colors

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Palette from './palette.js';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers.js';
 
 class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[3]} />
+        <Palette {...seedColors[4]} />
       </div>
     );
   }
