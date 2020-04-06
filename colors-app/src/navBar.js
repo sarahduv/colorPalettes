db5cd71bbdf40b70452051ccf6be1d360ce149bf
sarahduv <sarah.duvdevani@gmail.com>
2020-04-06 10:25:50 -0700

    added select drop down

--- colors-app/src/navBar.js
+++ colors-app/src/navBar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './navBar.css';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: 'hex' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, handleChange } = this.props;
+    const { format } = this.state;
     return (
       <header className="navBar">
         <div className="logo">
@@ -22,6 +35,13 @@ class NavBar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={this.state.format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="hex">rgb - 255,255,255</MenuItem>
+            <MenuItem value="hex">rgba - 255,255,255, 1.0</MenuItem>
+          </Select>
         </div>
       </header>
     );
