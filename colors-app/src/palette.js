eb2b1d66e0080a1a6d8e8565eca6c1e89cfbcff9
sarahduv <sarah.duvdevani@gmail.com>
2020-04-10 06:53:44 -0700

    fixing typos

--- colors-app/src/palette.js
+++ colors-app/src/palette.js
@@ -17,7 +17,7 @@ class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors, palletteName, emoji } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
       <ColorBox background={color[format]} name={color.name} key={color.id} />
@@ -30,8 +30,8 @@ class Palette extends Component {
           handleChange={this.changeFormat}
         />
         <div className="Palette-colors">{colorBoxes}</div>
-        <footer className="Pallete-footer">
-          {palletteName}
+        <footer className="Palette-footer">
+          {paletteName}
           <span className="emoji">{emoji}</span>
         </footer>
       </div>
